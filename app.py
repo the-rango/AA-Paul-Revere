30095dc92e21bfb1b0c02c5d499854a63cb64ced
Bryce <goldb@uci.edu>
2019-06-27 17:53:08 -0700

    Added push notification endpoint

--- app.py
+++ app.py
@@ -57,16 +57,19 @@ def add_email(code, name, email):
     name = urllib.parse.unquote(name)
 
     doc = db["queue"].find_one({"code": code})
+    
     if doc is None: #course not in db yet
-        db["queue"].insert_one({"code": code, "name":name, "emails": [email], "nums": []})
+        db["queue"].insert_one({"code": code, "name":name, "emails": [email], "nums": [], "push": []})
+
     elif email not in doc["emails"]: #email not added already
         doc["emails"].append(email)
         db["queue"].find_one_and_update({'_id': doc['_id']}, {"$set": doc})
+
     else: #already in the db
-        msg = '{} was already on the email watchlist for {} {}!'.format(email,code,name)
+        msg = '{} was already on the email watchlist for {} {}!'.format(email, code, name)
         return render_template("landing.html", img_link = "https://www.ics.uci.edu/~rang1/PRL/bg_img/bg{}.jpg".format(lucky), message=msg, credits=CREDITS[lucky])
 
-    msg = '{} has been added to the email watchlist for {} {}!</h1></body></html>'.format(email,code,name)
+    msg = '{} has been added to the email watchlist for {} {}!</h1></body></html>'.format(email, code, name)
     return render_template("landing.html", img_link = "https://www.ics.uci.edu/~rang1/PRL/bg_img/bg{}.jpg".format(lucky), message=msg, credits=CREDITS[lucky])
 
 @app.route("/sms/<code>/<name>/<num>")
@@ -75,15 +78,40 @@ def add_sms(code, name, num):
     name = urllib.parse.unquote(name)
 
     doc = db["queue"].find_one({"code": code})
+
     if doc is None: #course not in db yet
-        db["queue"].insert_one({"code": code, "name":name, "emails": [], "nums": [num]})
+        db["queue"].insert_one({"code": code, "name":name, "emails": [], "nums": [num], "push": []})
+
     elif num not in doc["nums"]: #number not added already
         doc["nums"].append(num)
         db["queue"].find_one_and_update({'_id': doc['_id']}, {"$set": doc})
+
     else: #already in the db
-        msg = '{} was already on the sms watchlist for {} {}!'.format(num,code,name)
+        msg = '{} was already on the sms watchlist for {} {}!'.format(num, code, name)
         return render_template("landing.html", img_link = "https://www.ics.uci.edu/~rang1/PRL/bg_img/bg{}.jpg".format(lucky), message=msg, credits=CREDITS[lucky])
-    msg = '{} has been added to the sms watchlist for {} {}!'.format(num,code,name)
+
+    msg = '{} has been added to the sms watchlist for {} {}!'.format(num, code, name)
+    return render_template("landing.html", img_link = "https://www.ics.uci.edu/~rang1/PRL/bg_img/bg{}.jpg".format(lucky), message=msg, credits=CREDITS[lucky])
+
+@app.route("/pushnotif/<code>/<name>/<token>")
+def add_push_notif(code, name, token):
+    lucky = random.randrange(0, len(CREDITS))
+    name = urllib.parse.unquote(name)
+
+    doc = db["queue"].find_one({"code": code})
+    
+    if doc is None: #course not in db yet
+        db["queue"].insert_one({"code": code, "name":name, "emails": [], "nums": [], "push": [token]})
+
+    elif token not in doc["push"]: #token not added already
+        doc["push"].append(token)
+        db["queue"].find_one_and_update({'_id': doc['_id']}, {"$set": doc})
+
+    else: #already in the db
+        msg = '{} was already on the push notification watchlist for {} {}!'.format(token, code, name)
+        return render_template("landing.html", img_link = "https://www.ics.uci.edu/~rang1/PRL/bg_img/bg{}.jpg".format(lucky), message=msg, credits=CREDITS[lucky])
+
+    msg = '{} has been added to the push notification watchlist for {} {}!</h1></body></html>'.format(token, code, name)
     return render_template("landing.html", img_link = "https://www.ics.uci.edu/~rang1/PRL/bg_img/bg{}.jpg".format(lucky), message=msg, credits=CREDITS[lucky])
 
 if __name__ == '__main__':
