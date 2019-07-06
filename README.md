e7cd32ebd24a822c1bd4044a3cceac3707850e6e
Ran Gu <earlguwang@gmail.com>
2019-07-05 23:12:30 -0700

    Clearer org + documentation
    And gave credit to @suzannachen for making dispatcher more efficient!
    
    Co-Authored-By: suzannachen <suzannachen@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# AA-Paul-Revere
-Lets you know when the British are coming. Just kidding. Mails you notifications for when classes(waitlists) open.
+# Paul Revere
+Lets you know when the British are coming. Just kidding. Notifies you when classes or waitlists open with email (SendGrid), text (AWS SNS), and push notification (Firebase). Paul works with AntAlmanac for UCI.
+
+There are actually two programs here, that do separate jobs but work together to make Paul Revere work. We used a MongoDB datastore for the watchlist.
+
+## Bookkeeper
+The bookkeeper is a Flask API that accepts requests to add courses to watchlist. Even though it is mostly called by AntAlmanac programmatically, users may view it when using the add-back link that comes with notifications. For these landing pages, we used amazing UCI landscape photography taken by students.
+
+It also includes an endpoint to send test push notifications to verify that the user is on a compatible device.
+
+#### Usage
+GET request to /<type>/<code>/<name>/<address>, where type is 'email', 'sms', or 'push', code is the 5 digit section code, name is the name of the course, and address is the email address, phone number of push token to be added to the database.
+
+## Dispatcher
+The dispatcher checks if the courses on the watchlist have opened, and if any has, it sends out the appropriate notifications.
+
+This is done concurrently with Paul and his doggo, one checks the watchlist and sends notifications, while the other one fetches course statuses. Paul and doggo communicate via pickling. We preferred this over a database because it was one fewer thing to set up (and break) and because pickling is surprisingly efficient.
+
+We parse [UCI Registrar's Schedule of Classes (SOC)](https://www.reg.uci.edu/perl/WebSoc) with BeautifulSoup which uses lxml under the hood.
+
+Overall, it takes generally less than 15 seconds to receive a push notification from the moment when a course is dropped, anecdotally.
+
+#### Usage
+Run both paul.py and doggo.py as separate processes.
---
+++ bookkeeper/app.py
@@ -0,0 +1,137 @@
+from flask import Flask, render_template, request
+from flask_cors import CORS
+import urllib
+import pymongo
+import config
+import random
+import requests
+
+app = Flask(__name__)
+CORS(app)
+db = pymongo.MongoClient(config.MONGODB_URI).get_default_database()
+
+CREDITS = [
+    '''
+    Photo Credit: Emre Erdogmus<br>
+    Shot on iPhone XR<br>
+    IG Photography account: <a href="https://www.instagram.com/emresfotos/" target="_blank">@emresfotos</a>
+    ''',
+    '''
+    Photo Credit: Lavanya Kukkemane<br>
+    Instagram: @lavanyak814
+    ''',
+    '''
+    Photo Credit: Calvin Harris Belcher
+    ''',
+    '''
+    Photo Credit: Benedict Chua
+    ''',
+    '''
+    Photo Credit: Benedict Chua
+    ''',
+    '''
+    Photo Credit: Benedict Chua
+    ''',
+    '''
+    Photo Credit: Benedict Chua
+    ''',
+    '''
+    Photo Credit: Benedict Chua
+    ''',
+    '''
+    Photo Credit: Benedict Chua
+    ''',
+    '''
+    Photo Credit: Benedict Chua
+    ''',
+    '''
+    Photo Credit: Benedict Chua
+    ''',
+    '''
+    Photo Credit: Benedict Chua
+    '''
+]
+
+@app.route("/email/<code>/<name>/<email>")
+def add_email(code, name, email):
+    lucky = random.randrange(0, len(CREDITS))
+    name = urllib.parse.unquote(name)
+
+    doc = db["queue"].find_one({"code": code})
+
+    if doc is None: #course not in db yet
+        db["queue"].insert_one({"code": code, "name":name, "emails": [email], "nums": [], "push": []})
+
+    elif email not in doc["emails"]: #email not added already
+        doc["emails"].append(email)
+        db["queue"].find_one_and_update({'_id': doc['_id']}, {"$set": doc})
+
+    else: #already in the db
+        msg = '{} was already on the email watchlist for {} {}!'.format(email, code, name)
+        return render_template("landing.html", img_link = "https://www.ics.uci.edu/~rang1/PRL/bg_img/bg{}.jpg".format(lucky), message=msg, credits=CREDITS[lucky])
+
+    msg = '{} has been added to the email watchlist for {} {}!</h1></body></html>'.format(email, code, name)
+    return render_template("landing.html", img_link = "https://www.ics.uci.edu/~rang1/PRL/bg_img/bg{}.jpg".format(lucky), message=msg, credits=CREDITS[lucky])
+
+@app.route("/sms/<code>/<name>/<num>")
+def add_sms(code, name, num):
+    lucky = random.randrange(len(CREDITS))
+    name = urllib.parse.unquote(name)
+
+    doc = db["queue"].find_one({"code": code})
+
+    if doc is None: #course not in db yet
+        db["queue"].insert_one({"code": code, "name":name, "emails": [], "nums": [num], "push": []})
+
+    elif num not in doc["nums"]: #number not added already
+        doc["nums"].append(num)
+        db["queue"].find_one_and_update({'_id': doc['_id']}, {"$set": doc})
+
+    else: #already in the db
+        msg = '{} was already on the sms watchlist for {} {}!'.format(num, code, name)
+        return render_template("landing.html", img_link = "https://www.ics.uci.edu/~rang1/PRL/bg_img/bg{}.jpg".format(lucky), message=msg, credits=CREDITS[lucky])
+
+    msg = '{} has been added to the sms watchlist for {} {}!'.format(num, code, name)
+    return render_template("landing.html", img_link = "https://www.ics.uci.edu/~rang1/PRL/bg_img/bg{}.jpg".format(lucky), message=msg, credits=CREDITS[lucky])
+
+@app.route("/pushnotif/<code>/<name>/<token>")
+def add_push_notif(code, name, token):
+    lucky = random.randrange(0, len(CREDITS))
+    name = urllib.parse.unquote(name)
+
+    doc = db["queue"].find_one({"code": code})
+
+    if doc is None: #course not in db yet
+        db["queue"].insert_one({"code": code, "name":name, "emails": [], "nums": [], "push": [token]})
+
+    elif token not in doc["push"]: #token not added already
+        doc["push"].append(token)
+        db["queue"].find_one_and_update({'_id': doc['_id']}, {"$set": doc})
+
+    else: #already in the db
+        msg = '{} was already on the push notification watchlist for {} {}!'.format(token, code, name)
+        return render_template("landing.html", img_link = "https://www.ics.uci.edu/~rang1/PRL/bg_img/bg{}.jpg".format(lucky), message=msg, credits=CREDITS[lucky])
+
+    msg = '{} has been added to the push notification watchlist for {} {}!</h1></body></html>'.format(token, code, name)
+    return render_template("landing.html", img_link = "https://www.ics.uci.edu/~rang1/PRL/bg_img/bg{}.jpg".format(lucky), message=msg, credits=CREDITS[lucky])
+
+@app.route("/testpush/<token>")
+def test_push(token):
+    fcm = "https://fcm.googleapis.com/fcm/send"
+    headers = {"Authorization": config.PUSH_AUTHKEY,
+                "Content-Type": "application/json"}
+    body = {
+        "notification": {
+            "title": "AntAlmanac Test Notification",
+            "body": "This is a test push notification to make sure you can get it. Click for more details.",
+            "click_action": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            "icon": "https://www.ics.uci.edu/~rang1/AntAlmanac/img/LogoSquareWhite.png"
+        },
+        "to": token
+        }
+    r = requests.post(fcm, data=json.dumps(body), headers=headers)
+    return render_template("landing.html", img_link = "https://www.ics.uci.edu/~rang1/PRL/bg_img/bg{}.jpg".format(0), message='Token push test sent', credits=CREDITS[0])
+
+if __name__ == '__main__':
+    # app.debug = True
+    app.run()
